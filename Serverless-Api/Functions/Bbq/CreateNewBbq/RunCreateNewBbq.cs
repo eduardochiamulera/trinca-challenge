2b5c9806f4c9c45932fdc9ecad5532a92c4ee0dc
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-04 21:37:11 -0300

    usando repositorio para criar um novo churrasco

--- Serverless-Api/Functions/Bbq/CreateNewBbq/RunCreateNewBbq.cs
+++ Serverless-Api/Functions/Bbq/CreateNewBbq/RunCreateNewBbq.cs
@@ -5,50 +5,52 @@ using Domain.Events;
 using Domain.Entities;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Domain.Repositories;
 
 namespace Serverless_Api
 {
     public partial class RunCreateNewBbq
     {
-        private readonly Person _user;
-        private readonly SnapshotStore _snapshots;
-        private readonly IEventStore<Bbq> _bbqsStore;
-        private readonly IEventStore<Person> _peopleStore;
-        public RunCreateNewBbq(IEventStore<Bbq> eventStore, IEventStore<Person> peopleStore, SnapshotStore snapshots, Person user)
-        {
-            _user = user;
-            _snapshots = snapshots;
-            _bbqsStore = eventStore;
-            _peopleStore = peopleStore;
-        }
+		private readonly Person _user;
+		private readonly SnapshotStore _snapshots;
+		private readonly IPersonRepository _personRepository;
+		private readonly IBbqRepository _bbqRepository;
+		public RunCreateNewBbq(IBbqRepository bbqRepository, IPersonRepository personsRepository, SnapshotStore snapshots, Person user)
+		{
+			_user = user;
+			_snapshots = snapshots;
+			_personRepository = personsRepository;
+			_bbqRepository = bbqRepository;
+		}
 
-        [Function(nameof(RunCreateNewBbq))]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "churras")] HttpRequestData req)
-        {
-            var input = await req.Body<NewBbqRequest>();
+		[Function(nameof(RunCreateNewBbq))]
+		public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "churras")] HttpRequestData req)
+		{
+			var input = await req.Body<NewBbqRequest>();
 
-            if (input == null)
-            {
-                return await req.CreateResponse(HttpStatusCode.BadRequest, "input is required.");
-            }
+			if (input == null)
+			{
+				return await req.CreateResponse(HttpStatusCode.BadRequest, "input is required.");
+			}
 
-            var churras = new Bbq();
-            churras.Apply(new ThereIsSomeoneElseInTheMood(Guid.NewGuid(), input.Date, input.Reason, input.IsTrincasPaying));
+			var churras = new Bbq();
 
-            await _bbqsStore.WriteToStream(churras.Id, churras.Changes.Select(evento => new EventData(churras.Id, evento, new { CreatedBy = _user.Id }, churras.Version, DateTime.Now.ToString())).ToArray(), expectedVersion: churras.Version == 0 ? null : churras.Version);
+			churras.Apply(new ThereIsSomeoneElseInTheMood(Guid.NewGuid(), input.Date, input.Reason, input.IsTrincasPaying));
 
-            var churrasSnapshot = churras.TakeSnapshot();
+			var churrasSnapshot = churras.TakeSnapshot();
 
-            var Lookups = await _snapshots.AsQueryable<Lookups>("Lookups").SingleOrDefaultAsync();
+			var Lookups = await _snapshots.AsQueryable<Lookups>("Lookups").SingleOrDefaultAsync();
 
-            foreach (var personId in Lookups.ModeratorIds)
-            {
-                var header = await _peopleStore.ReadHeader(personId);
-                var @event = new PersonHasBeenInvitedToBbq(churras.Id, churras.Date, churras.Reason);
-                await _peopleStore.WriteToStream(personId, new[] { new EventData(personId, @event, new { CreatedBy = _user.Id }, header.StreamHeader.Version, DateTime.Now.ToString()) }, expectedVersion: header.StreamHeader.Version == 0 ? null : header.StreamHeader.Version);
-            }
+			await _bbqRepository.SaveAsync(churras);
 
-            return await req.CreateResponse(HttpStatusCode.Created, churrasSnapshot);
-        }
-    }
+			foreach (var personId in Lookups.ModeratorIds)
+			{
+				var person = await _personRepository.GetAsync(personId);
+				person.Apply(new PersonHasBeenInvitedToBbq(churras.Id, churras.Date, churras.Reason));
+				await _personRepository.SaveAsync(person);
+			}
+
+			return await req.CreateResponse(HttpStatusCode.Created, churrasSnapshot);
+		}
+	}
 }
