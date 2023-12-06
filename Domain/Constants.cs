1c85ba7234bfeac93bdd8112dccdfe14e271b1f2
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-05 21:21:23 -0300

    adicionado constantes

---
+++ Domain/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+	public static class Constants
+	{
+		public const int NumeroConfirmacoesAlteraStatusBbq = 7;
+		public const decimal QuantidadeCarneKilos = 0.3m;
+		public const decimal QuantidadeVegetaisVegetarianosKilos = 0.6m;
+		public const decimal QuantidadeVegetaisKilos = 0.3m;
+	}
+}
