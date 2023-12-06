b1ca06233ecdcec6f991ec15ac5c25ef03bbfdec
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-05 21:21:01 -0300

    atualizar bbq automaticamente

---
+++ Domain/Events/BbqStatusUpdatedAutomatic.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Events
+{
+    public class BbqStatusUpdatedAutomatic : IEvent
+    {
+        public BbqStatusUpdatedAutomatic(BbqStatus status)
+        {
+            Status = status;
+        }
+
+        public BbqStatus Status { get; }
+    }
+}
