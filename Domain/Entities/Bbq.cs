c93a45ccab8870556d50c8c0a21fb51340d647eb
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-05 21:20:34 -0300

    adicionado lista de compras e quantidade de pessoas confirmadas

--- Domain/Entities/Bbq.cs
+++ Domain/Entities/Bbq.cs
@@ -1,44 +1,75 @@
-﻿using System;
-using Domain.Events;
+﻿using Domain.Events;
+using System;
+using System.Dynamic;
 
 namespace Domain.Entities
 {
-    public class Bbq : AggregateRoot
-    {
-        public string Reason { get; set; }
-        public BbqStatus Status { get; set; }
-        public DateTime Date { get; set; }
-        public bool IsTrincasPaying { get; set; }
-        public void When(ThereIsSomeoneElseInTheMood @event)
-        {
-            Id = @event.Id.ToString();
-            Date = @event.Date;
-            Reason = @event.Reason;
-            Status = BbqStatus.New;
-        }
+	public class Bbq : AggregateRoot
+	{
+		public string Reason { get; set; }
+		public BbqStatus Status { get; set; }
+		public DateTime Date { get; set; }
+		public bool IsTrincasPaying { get; set; }
+		public int BbqConfirmation { get; private set; }
+		public ShoppingList ShoppingList { get; private set; }
+		public Bbq()
+		{
+			ShoppingList = new ShoppingList();
+		}
 
-        public void When(BbqStatusUpdated @event)
-        {
-            if (@event.GonnaHappen)
-                Status = BbqStatus.PendingConfirmations;
-            else 
-                Status = BbqStatus.ItsNotGonnaHappen;
+		public void When(ThereIsSomeoneElseInTheMood @event)
+		{
+			Id = @event.Id.ToString();
+			Date = @event.Date;
+			Reason = @event.Reason;
+			Status = BbqStatus.New;
+		}
 
-            if (@event.TrincaWillPay)
-                IsTrincasPaying = true;
-        }
+		public void When(BbqStatusUpdatedAutomatic @event)
+		{
+			Status = @event.Status;
+		}
 
-        public void When(InviteWasDeclined @event)
-        {
-            //TODO:Deve ser possível rejeitar um convite já aceito antes.
-            //Se este for o caso, a quantidade de comida calculada pelo aceite anterior do convite
-            //deve ser retirado da lista de compras do churrasco.
-            //Se ao rejeitar, o número de pessoas confirmadas no churrasco for menor que sete,
-            //o churrasco deverá ter seu status atualizado para “Pendente de confirmações”. 
-        }
+		public void When(BbqStatusUpdated @event)
+		{
+			Status = @event.Status;
+			IsTrincasPaying = @event.TrincaWillPay;
+		}
 
-        public object TakeSnapshot()
-        {
+		public void When(InviteWasDeclined @event)
+		{
+			if (BbqConfirmation == default)
+			{
+				return;
+			}
+
+			BbqConfirmation -= 1;
+			if (@event.IsVeg)
+			{
+				ShoppingList.UpdateBbqListShop((Constants.QuantidadeVegetaisVegetarianosKilos * -1), 0, ShoppingListCalculationType.Decrement);
+			}
+			else
+			{
+				ShoppingList.UpdateBbqListShop(Constants.QuantidadeVegetaisKilos, Constants.QuantidadeCarneKilos, ShoppingListCalculationType.Decrement);
+			}
+		}
+
+		public void When(InviteWasAccepted @event)
+		{
+			BbqConfirmation += 1;
+			if (@event.IsVeg)
+			{
+				ShoppingList.UpdateBbqListShop(Constants.QuantidadeVegetaisVegetarianosKilos, 0, ShoppingListCalculationType.Add);
+			}
+			else
+			{
+				ShoppingList.UpdateBbqListShop(Constants.QuantidadeVegetaisKilos, Constants.QuantidadeCarneKilos, ShoppingListCalculationType.Add);
+			}
+		}
+
+
+		public object TakeSnapshot()
+		{
             return new
             {
                 Id,
@@ -46,6 +77,6 @@ namespace Domain.Entities
                 IsTrincasPaying,
                 Status = Status.ToString()
             };
-        }
-    }
+		}
+	}
 }
---
+++ Domain/Entities/ShoppingList.cs
@@ -0,0 +1,37 @@
+﻿namespace Domain.Entities
+{
+	public class ShoppingList
+	{
+		public decimal Vegetables { get; private set; }
+		public decimal Meat { get; private set; }
+
+		internal void UpdateBbqListShop(decimal vegetables, decimal meat, ShoppingListCalculationType type)
+		{
+			if(type == ShoppingListCalculationType.Add)
+			{
+				Vegetables += vegetables;
+				Meat += meat;
+			}
+			else
+			{
+				Vegetables -= vegetables;
+				Meat -= meat;
+			}
+		}
+
+		public object? TakeSnapshot()
+		{
+			return new
+			{
+				Vegetables,
+				Meat
+			};
+		}
+	}
+
+	public enum ShoppingListCalculationType
+	{
+		Add,
+		Decrement
+	}
+}
