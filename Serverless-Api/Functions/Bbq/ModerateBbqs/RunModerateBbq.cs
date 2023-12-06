fc9d3cb7eba4cad921fddd11cc76d52a96f40cd3
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-06 20:41:23 -0300

    declinar os convites se o churrasco nao foi aceito

--- Serverless-Api/Functions/Bbq/ModerateBbqs/RunModerateBbq.cs
+++ Serverless-Api/Functions/Bbq/ModerateBbqs/RunModerateBbq.cs
@@ -8,49 +8,62 @@ using System.Net;
 
 namespace Serverless_Api
 {
-    public partial class RunModerateBbq
-    {
-        private readonly SnapshotStore _snapshots;
-        private readonly IPersonRepository _persons;
-        private readonly IBbqRepository _repository;
+	public partial class RunModerateBbq
+	{
+		private readonly SnapshotStore _snapshots;
+		private readonly IPersonRepository _persons;
+		private readonly IBbqRepository _repository;
 
-        public RunModerateBbq(IBbqRepository repository, SnapshotStore snapshots, IPersonRepository persons)
-        {
-            _persons = persons;
-            _snapshots = snapshots;
-            _repository = repository;
-        }
+		public RunModerateBbq(IBbqRepository repository, SnapshotStore snapshots, IPersonRepository persons)
+		{
+			_persons = persons;
+			_snapshots = snapshots;
+			_repository = repository;
+		}
 
-        [Function(nameof(RunModerateBbq))]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "put", Route = "churras/{id}/moderar")] HttpRequestData req, string id)
-        {
-            var moderationRequest = await req.Body<ModerateBbqRequest>();
+		[Function(nameof(RunModerateBbq))]
+		public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "put", Route = "churras/{id}/moderar")] HttpRequestData req, string id)
+		{
+			var moderationRequest = await req.Body<ModerateBbqRequest>();
 
-            var bbq = await _repository.GetAsync(id);
+			var bbq = await _repository.GetAsync(id);
 
-            if(bbq == null)
-            {
+			if (bbq == null)
+			{
 				return await req.CreateResponse(HttpStatusCode.NotFound, "Bbq not found.");
 			}
 
-            bbq.Apply(new BbqStatusUpdated(moderationRequest.GonnaHappen, moderationRequest.TrincaWillPay));
+			bbq.Apply(new BbqStatusUpdated(moderationRequest.GonnaHappen, moderationRequest.TrincaWillPay));
+			await _repository.SaveAsync(bbq);
 
-            var lookups = await _snapshots.AsQueryable<Lookups>("Lookups").SingleOrDefaultAsync();
+			var lookups = await _snapshots.AsQueryable<Lookups>("Lookups").SingleOrDefaultAsync();
 
-            if (moderationRequest.GonnaHappen)
-            {
-                foreach (var personId in lookups.PeopleIds)
-                {
-                    var person = await _persons.GetAsync(personId);
-                    var @event = new PersonHasBeenInvitedToBbq(bbq.Id, bbq.Date, bbq.Reason);
-                    person.Apply(@event);
-                    await _persons.SaveAsync(person);
-                }
-            }
+			if (moderationRequest.GonnaHappen)
+			{
+				foreach (var personId in lookups.PeopleIds)
+				{
+					var person = await _persons.GetAsync(personId);
+					if (!person.IsCoOwner)
+					{
+						var @event = new PersonHasBeenInvitedToBbq(bbq.Id, bbq.Date, bbq.Reason);
+						person.Apply(@event);
+						await _persons.SaveAsync(person);
+					}
+				}
+			}
+			else
+			{
+				foreach (var personId in lookups.PeopleIds)
+				{
+					var person = await _persons.GetAsync(personId);
+					var @event = new InviteWasDeclined { InviteId = bbq.Id, PersonId = person.Id } ;
+					person.Apply(@event);
+					await _persons.SaveAsync(person);
+				}
+			}
 
-            await _repository.SaveAsync(bbq);
 
-            return await req.CreateResponse(HttpStatusCode.OK, bbq.TakeSnapshot());
-        }
-    }
+			return await req.CreateResponse(HttpStatusCode.OK, bbq.TakeSnapshot());
+		}
+	}
 }
