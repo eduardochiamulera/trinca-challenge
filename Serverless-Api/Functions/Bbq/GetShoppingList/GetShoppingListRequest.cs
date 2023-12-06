4d41453230b345ecd29c9f3265f381f64f862a53
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-05 21:21:51 -0300

    retornar lista de compra para moderadores

---
+++ Serverless-Api/Functions/Bbq/GetShoppingList/GetShoppingListRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Serverless_Api.Functions.Bbq.GetShoppingList
+{
+	public partial class RunGetShoppingList
+	{
+		public class GetShoppingListRequest
+		{
+			public GetShoppingListRequest(string bbqId)
+			{
+				BbqId = bbqId;
+			}
+
+			public string BbqId { get; }
+		}
+	}
+}
