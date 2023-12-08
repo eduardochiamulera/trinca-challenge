737f57e0c64e1bac1521e13d48caa99c3b748e79
Eduardo Fraga <eduardofraga1994@gmail.com>
2023-12-08 17:22:34 -0300

    adicionado classes de erro

--- Domain/Constants.cs
+++ Domain/Constants.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain
+﻿using CrossCutting.Models;
+
+namespace Domain
 {
 	public static class Constants
 	{
@@ -6,5 +8,9 @@
 		public const decimal QuantidadeCarneKilos = 0.3m;
 		public const decimal QuantidadeVegetaisVegetarianosKilos = 0.6m;
 		public const decimal QuantidadeVegetaisKilos = 0.3m;
+
+		public static Error InputRequired => BadRequest("Input is required");
+		public static Error BadRequest(string message) => new Error(400, message);
+		public static Error NotFound(string value) => new Error(404, $"{value} not found.");
 	}
 }
